b5246c5d50b35af1cefe5776ba6d0bce2fc19c50
Mr-Liu-Jie <31774239+Mr-Liu-Jie@users.noreply.github.com>
2019-01-11 09:59:27 +0800

    +更新所有商品id

--- src/main/java/cn/com/suosi/spider/xiaomimall/SpiderXiaomimallApplication.java
+++ src/main/java/cn/com/suosi/spider/xiaomimall/SpiderXiaomimallApplication.java
@@ -19,28 +19,32 @@ public class SpiderXiaomimallApplication {
     public static void main(String[] args) throws Exception {
 
 
-//        // 所有商品url
-//        List<String> productUrls = UrlFactory.getProductUrl(SysConstant.BASE_URL);
-//
-//        // 所有商品id
-//        List<String> productIds1 = new ArrayList<>();
-//
-//        for (String producturl : productUrls) {
-//            String productId = SearchUrlParse.getProductId(producturl);
-//            productIds1.add(productId);
-//        }
-//
-//        // 去重
-//        List<String> productIds = productIds1.stream().distinct().collect(Collectors.toList());
+        /**------------------------------更新所有商品id-------------------------------*/
 
+        // 所有商品url
+        List<String> productUrls = UrlFactory.getProductUrl(SysConstant.BASE_URL);
 
         // 所有商品id
+        List<String> productIds1 = new ArrayList<>();
+
+        for (String producturl : productUrls) {
+            String productId = SearchUrlParse.getProductId(producturl);
+            productIds1.add(productId);
+        }
+
+        // 去重
+        List<String> productIds2 = productIds1.stream().distinct().collect(Collectors.toList());
+
+
+        /**------------------------------更新所有商品信息------------------------------*/
+
+        // 所有商品id，常量PRODUCT_IDS
         List<String> productIds = SysConstant.PRODUCT_IDS;
 
         // 所有商品信息 + 评论
         List<Product> productInfoLists = new ArrayList<>();
 
-        for (String productid : productIds) {
+        for (String productid : productIds2) {
 
             List<Product> productInfoList = new ArrayList<>();
 
@@ -79,6 +83,12 @@ public class SpiderXiaomimallApplication {
         // 存入数据库
         MySqlController.executeInsert(productInfoLists);
 
+
+        /**----------------------------------检验是否更新所有商品id-------------------------------------*/
+        System.out.println("productIds2 = " + productIds2);
+        System.out.println(productIds2.size());
+        System.out.println("productIds = " + productIds);
+        System.out.println(productIds.size());
     }
 }
 
